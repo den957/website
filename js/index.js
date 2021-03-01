0542701ff83c56527b7c6776dc6410bcbfa287ec
Dan 957 <denyshorban999@gamil.com>
2021-03-01 20:12:28 +0100

    Add opportunity to scrolle page by points

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
-    let burger = function () {
+    const burger = function () {
         let close = document.querySelector('.header__burger-close')
         let burger = document.querySelector('.header__burger-wrapper')
         let menu = document.querySelector('.header__menu')
@@ -16,16 +16,45 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     burger()
 
-    let scrollPageDown = function () {
-        let button = document.querySelector('.development__arrow-down')
-        button.addEventListener('click', () => {
+    const scrollPageDown = function () {
+        // receive buttons 
+        const arrowButton = document.querySelector('.development__arrow-down'),
+            homeButton = document.querySelector('.header__home'),
+            aboutButton = document.querySelector('.header__about'),
+            servisingButton = document.querySelector('.header__servising'),
+            portfolioButton = document.querySelector('.header__portfolio'),
+            pricingButton = document.querySelector('.header__pricing'),
+            teamButton = document.querySelector('.header__team')
+        // receive main elements (dividing our page into categories)
+        const aboutUs = document.querySelector('.about-us__body'),
+            services = document.querySelector('.services__body'),
+            portfolio = document.querySelector('.latest-work__body'),
+            pricingPlan = document.querySelector('.pricing-plan__body'),
+            teamMember = document.querySelector('.team__body')
+
+        // function animation 
+        const scrollPage = (position) => {
+            event.preventDefault()
             window.scroll({
                 left: 0,
-                top: 720,
+                top: position,
                 behavior: 'smooth'
             })
-        })
+        }
+        //cordinates 
+        const aboutCordinateY = document.body.scrollTop + aboutUs.getBoundingClientRect().top
+        const servicesCordinateY = document.body.scrollTop + services.getBoundingClientRect().top
+        const portfolioCordinateY = document.body.scrollTop + portfolio.getBoundingClientRect().top
+        const pricingPlanCordinateY = document.body.scrollTop + pricingPlan.getBoundingClientRect().top
+        const teamCordinateY = document.body.scrollTop + teamMember.getBoundingClientRect().top
+        //button click
+        arrowButton.addEventListener('click', () => { scrollPage(aboutCordinateY, event) })
+        homeButton.addEventListener('click', () => { scrollPage(document.body.scrollTop, event) })
+        aboutButton.addEventListener('click', () => { scrollPage(aboutCordinateY, event) })
+        servisingButton.addEventListener('click', () => { scrollPage(servicesCordinateY, event) })
+        portfolioButton.addEventListener('click', () => { scrollPage(portfolioCordinateY, event) })
+        pricingButton.addEventListener('click', () => { scrollPage(pricingPlanCordinateY, event) })
+        teamButton.addEventListener('click', () => { scrollPage(teamCordinateY, event) })
     }
     scrollPageDown()
 })
-
