a3e2cd76072d72dbec89cc99f14642a38ba95d24
Dan 957 <denyshorban999@gamil.com>
2021-03-01 16:51:05 +0100

    Add function scroll down

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
-let close = document.querySelector('.header__burger-close')
-let burger = document.querySelector('.header__burger-wrapper')
-let menu = document.querySelector('.header__menu')
-close.addEventListener('click', () => {
-    close.style.display = 'none'
-    menu.style.display = 'none'
-    burger.style.display = 'block'
-})
-burger.addEventListener('click', () => {
-    burger.style.display = 'none'
-    close.style.display = 'block'
-    menu.style.display = 'block'
+window.addEventListener('DOMContentLoaded', function () {
+    let burger = function () {
+        let close = document.querySelector('.header__burger-close')
+        let burger = document.querySelector('.header__burger-wrapper')
+        let menu = document.querySelector('.header__menu')
+        close.addEventListener('click', () => {
+            close.style.display = 'none'
+            menu.style.display = 'none'
+            burger.style.display = 'block'
+        })
+        burger.addEventListener('click', () => {
+            burger.style.display = 'none'
+            close.style.display = 'block'
+            menu.style.display = 'block'
+        })
+    }
+    burger()
+
+    let scrollPageDown = function () {
+        let button = document.querySelector('.development__arrow-down')
+        button.addEventListener('click', () => {
+            window.scroll({
+                left: 0,
+                top: 720,
+                behavior: 'smooth'
+            })
+        })
+    }
+    scrollPageDown()
 })
 
